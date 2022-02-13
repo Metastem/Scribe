24d3ab9ab34634f640a73443ec966c4e3860e3bd
Edward Loveall <edward@edwardloveall.com>
2022-02-13 10:08:16 -0500

    Better article ID parsing
    A new ArticleIdParser class takes in an HTTP::Request object and parses
    the article ID from it. It intentinoally fails on tag, user, and search
    pages and attempts to only catch articles.

--- src/actions/articles/show.cr
+++ src/actions/articles/show.cr
@@ -2,7 +2,7 @@ require "json"
 
 class Articles::Show < BrowserAction
   fallback do
-    post_id = maybe_post_id(context.request)
+    post_id = ArticleIdParser.parse(context.request)
     case post_id
     in Monads::Just
       response = client_class.post_data(post_id.value!)
@@ -16,27 +16,6 @@ class Articles::Show < BrowserAction
         original_path: request.path,
       )
     end
-  end
-
-  def maybe_post_id(request : HTTP::Request)
-    from_params = post_id_from_params(request.query_params)
-    from_path = post_id_from_path(request.path)
-    from_path.or(from_params)
-  end
-
-  def post_id_from_path(request_path : String)
-    Monads::Try(Regex::MatchData)
-      .new(->{ request_path.match(/([0-9a-f]+)$/i) })
-      .to_maybe
-      .fmap(->(matches : Regex::MatchData) { matches[1] })
-  end
-
-  def post_id_from_params(params : URI::Params)
-    maybe_uri = Monads::Try(String)
-      .new(->{ params["redirectUrl"] })
-      .to_maybe
-      .fmap(->(url : String) { URI.parse(url) })
-      .bind(->(uri : URI) { post_id_from_path(uri.path) })
   end
 
   def client_class
